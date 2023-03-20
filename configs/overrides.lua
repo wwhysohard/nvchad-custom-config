1fe8262330050ff686402a60c67ebfba7cdaf69e
wwhysohard <47knursafi@gmail.com>
2023-03-20 17:57:36 +0600

    initial commit

---
+++ configs/overrides.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "elixir",
+    "heex",
+    "eex"
+  },
+  indent = {
+    enable = true,
+    disable = {
+      "python"
+    },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- elixir stuff
+    "elixir-lsp",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "deno",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
---
+++ plugins.lua
@@ -0,0 +1,68 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require("custom.configs.null-ls")
+        end,
+      },
+      {
+        "williamboman/mason.nvim",
+        config = function(_, opts)
+          dofile(vim.g.base46_cache .. "mason")
+          require("mason").setup(opts)
+          vim.api.nvim_create_user_command("MasonInstallAll", function()
+            vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+          end, {})
+          require("custom.configs.lspconfig") -- Load in lsp config
+        end,
+      },
+      "williamboman/mason-lspconfig.nvim"
+    },
+    config = function() end, -- Override to setup mason-lspconfig
+  },
+
+  -- overrde plugin configs
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- Uncomment if you want to re-enable which-key
+  -- {
+  --   "folke/which-key.nvim",
+  --   enabled = true,
+  -- },
+}
+
+return plugins
